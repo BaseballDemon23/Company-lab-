f8245224be0ddd5f009868a6c74b8bbf382fa94f
BaseballDemon23 <ansh13pat@gmail.com>
2025-05-21 08:27:58 -0400

    Create communications.h

---
+++ communications.h
@@ -0,0 +1,16 @@
+#ifndef Communications_h
+#define Communications_h
+
+#include <string>
+#include <algorithm>
+
+
+std::string greetCustomer(const std::string& name); 
+
+
+void formatAddress(std::string& address); 
+
+
+std::string closingMessage(const std::string& productName);
+
+#endif
